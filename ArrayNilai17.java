4020064fa044c6e768500699c19945d4455267a5
daffifiros <daffikun10@gmail.com>
2024-11-01 13:41:59 +0700

    Hasil Modifikasi 2

--- ArrayNilai17.java
+++ ArrayNilai17.java
@@ -4,12 +4,12 @@ public class ArrayNilai17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+ " : ");
             nilaiAkhir[i] = sc.nextInt();
             }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" +i+ " adalah " +nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.println("Nilai akhir ke-" +i+ " Lulus!");
         }
     }
 }
